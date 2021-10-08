c5188cf5691285f66e3608c34b11788ec052f2b1
Tcsekhar74 <46112464+Tcsekhar74@users.noreply.github.com>
2021-10-08 09:29:45 -0500

    Create bids.sql

---
+++ bids.sql
@@ -0,0 +1,16 @@
+/****** Object:  Table [hackathon].[bids]    Script Date: 10/8/2021 9:29:32 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [hackathon].[bids](
+	[OFFER_STATUS] [varchar](100) NULL,
+	[OpportunityID] [varchar](18) NULL,
+	[BIDDER_COMPANY_NAME] [varchar](255) NULL,
+	[companyid] [int] NULL
+) ON [PRIMARY]
+GO
+
+
